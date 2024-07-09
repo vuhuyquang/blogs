ab17603ee4c67d6ef797828c11f67d7162a32683
quangvh18 <quangvh18@ghtk.co>
2024-07-09 18:59:41 +0700

    update

--- pages/bai-viet/[slug].vue
+++ pages/bai-viet/[slug].vue
@@ -32,18 +32,21 @@
         </div>
       </div>
       <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-10 gap-y-12">
-        <div v-for="post in item.data.data" :key="post.id" class="flex flex-col cursor-pointer gap-3 p-4 border-2 border-gray-100 rounded-3xl">
+        <div v-for="post in item.data.data" :key="post.id"
+          class="flex flex-col cursor-pointer gap-3 p-4 border-2 border-gray-100 rounded-3xl">
           <div class="h-auto max-h-56 overflow-hidden rounded-2xl">
             <img class="h-full w-full zoom-on-hover rounded-2xl" src="https://wallpaperaccess.com/full/14736.jpg"
               alt="image">
           </div>
           <div class="px-1 flex flex-col gap-3">
             <div
-              class="text-sm lg:text-base xl:text-lg font-bold text-[#475467] hover:text-[#000000cc] transition delay-[45ms]">{{ post.title }}</div>
-            <div class="text-[10px] lg:text-[12px] xl:text-[14px] text-[#475467]">{{ post.short_content }}</div>
+              class="text-sm lg:text-base xl:text-lg font-bold text-[#475467] hover:text-[#000000cc] transition delay-[45ms]">
+              {{ post.title }}</div>
+            <div class="text-[10px] lg:text-[12px] xl:text-[14px] text-[#475467] line-clamp-2">{{ post.short_content }}
+            </div>
             <div class="flex justify-start gap-5 text-[#979797] items-center text-[7px] lg:text-[8px] xl:text-[10px]">
-              <span class="rounded-full bg-[#f2f2f2] px-2 py-1 flex justify-between items-center">Công nghệ</span>
-              <span>{{ post.created_at }}</span>
+              <span class="rounded-full bg-[#f2f2f2] px-2 py-1 flex justify-between items-center">{{ post.category_name }}</span>
+              <span>{{ formatDate(post.created_at) }}</span>
               <span>{{ post.reading_time }} phút đọc</span>
             </div>
           </div>
@@ -58,6 +61,22 @@
 </template>
 
 <script lang="ts" setup>
+import { formatDate } from '~/composables/ultils';
+
 const route = useRoute();
 const item = await getListPost(1, 20, null);
+
+useHead({
+  title: 'Bài viết',
+});
 </script>
+
+<style scoped>
+.line-clamp-2 {
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+</style>
